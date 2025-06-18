92887e118aaebc25b8c14a0089c81054e25c9439
Divyu <divyendureddy@gmail.com>
2025-06-18 17:28:45 +0530

    Write functions for obtaining required data from the API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function cleanData(data) {
+    curr = data["currentConditions"];
+    return {"temp": curr["temp"], "cond": curr["conditions"], "qnh": String(curr["pressure"]).padStart(4, "0"), "icon": curr["icon"]};
+}
+
+async function getData(city) {
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=3B98CBJ37XK6NVTBSR7BKBEBV&unitGroup=metric`, {mode: 'cors'});
+    let data = await response.json();
+    return cleanData(data);
+}
