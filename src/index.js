3003e99f2f55a959fc6a7d45766f20891c1be5b0
Divyu <divyendureddy@gmail.com>
2025-06-19 22:29:23 +0530

    Fix webpack CSS issue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 function cleanData(data) {
     curr = data["currentConditions"];
     return {"temp": curr["temp"], "cond": curr["conditions"], "qnh": String(curr["pressure"]).padStart(4, "0"), "icon": curr["icon"]};
