f2200af508b7ecaea22765414bc765a262559160
Divyu <divyendureddy@gmail.com>
2025-06-19 22:39:46 +0530

    Fix syntax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 function cleanData(data) {
-    curr = data["currentConditions"];
+    let curr = data["currentConditions"];
     return {"temp": curr["temp"], "cond": curr["conditions"], "qnh": String(curr["pressure"]).padStart(4, "0"), "icon": curr["icon"]};
 }
 
